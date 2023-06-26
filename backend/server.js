44dfe2efaae4f916de93a5329250047e335efc34
IT21158704 <it21158704@my.sliit.lk>
2023-06-26 14:38:59 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io')
+const User = require('../backend/models/User')
+const userRoutes = require('./routes/userRoutes')
 const io = new Server(server, {
     cors: '*',
     methods:'*'
@@ -13,7 +15,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
- 
+app.use('/users', userRoutes);
 
 server.listen(4040, () =>{
     console.log('server running at port', 4040)
